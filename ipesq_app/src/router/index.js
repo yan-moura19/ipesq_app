4da3c3b01d53c584e5ab189f0a38fa68007312b1
Yan Gabriel <43892743+yan-moura19@users.noreply.github.com>
2024-01-30 17:54:05 -0300

    projeto vue 3, axios, vue-router, vuex e vuetify 3

---
+++ ipesq_app/src/router/index.js
@@ -0,0 +1,16 @@
+/**
+ * router/index.ts
+ *
+ * Automatic routes for `./src/pages/*.vue`
+ */
+
+// Composables
+import { createRouter, createWebHistory } from 'vue-router/auto'
+
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  
+})
+
+export default router
